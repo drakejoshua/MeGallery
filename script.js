f8953d071cbc1ccf2518527ac4e50986418a4eb7
Joshua Mabawonku <drakejoshua086@gmail.com>
2023-12-16 19:25:02 +0100

    feat: (un)favourite image action and dialogs
    - added the favourite action to the favourite buttons on
      the image cards
    - added dialogs for the unfavourite action using sweetalert2.js
      alert functionality

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-// importing the download-buttons on the page as DOM elements for the script
+// import the download-buttons and favourite-buttons on the page as DOM elements for the script
 var downloadBtns = document.getElementsByClassName("download-btn");
+var favouriteBtns = document.getElementsByClassName("favourite-btn");
 
 
+// attach the sweetalert.js toast mechanism to the imported download buttons
 for ( var button of downloadBtns ) {
     button.addEventListener( "click", function() {
         Swal.fire(
@@ -23,5 +25,45 @@ for ( var button of downloadBtns ) {
                 showConfirmButton: false
             }
         );
+    });
+}
+
+
+for ( var favButton of favouriteBtns ) {
+    favButton.addEventListener( "click", function( event ) {
+        if ( event.target.classList.contains("bg-red-600") ) {
+            var alert = Swal.fire(
+                {
+                    title: "Confirm Action",
+                    text: "Are you sure you want to unfavourite this image?",
+                    icon: "warning",
+                    confirmButtonText: "Yes, I'm Sure",
+                    cancelButtonText: "No",
+                    showCancelButton: true,
+                    iconColor: "#60a5fa",
+                    confirmButtonColor: "#60a5fa",
+                    cancelButtonColor: "#60a5fa"
+                }
+            );
+
+
+            alert.then( function( alertResult ) {
+                if ( alertResult.isConfirmed ) {
+                    Swal.fire(
+                        {
+                            icon: "success",
+                            title: "Action Successful",
+                            text: "You've successfully unfavourited the image..!!",
+                            iconColor: "#60a5fa",
+                            confirmButtonColor: "#60a5fa"
+                        }
+                    );
+
+                    event.target.classList.remove("bg-red-600")
+                }
+            });
+        } else {
+            event.target.classList.add("bg-red-600")
+        }
     });
 }
