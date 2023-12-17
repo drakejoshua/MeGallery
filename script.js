a97297e65d5f63898e3b7f69ecc093914298b00b
Joshua Mabawonku <drakejoshua086@gmail.com>
2023-12-17 23:18:20 +0100

    style: added better comments
    - added comments to the javascript code as it was purely
      uncommented and badly-formatted.
    - also, marking the finish of the entire project.

--- script.js
+++ script.js
@@ -3,10 +3,17 @@ var downloadBtns = document.getElementsByClassName("download-btn");
 var favouriteBtns = document.getElementsByClassName("favourite-btn");
 
 
-// attach the sweetalert.js toast mechanism to the imported download buttons
+// attach the sweetalert.js toast alert mechanism and triggers to the imported download buttons 
+// on click
 for ( var button of downloadBtns ) {
+
+    // add "click" event listener to each download button
     button.addEventListener( "click", function() {
+
+        // trigger a toast alert immediately on click ( using sweetalert.js library )
         Swal.fire(
+
+            // the toast alert config( object )
             {
                 title: "Download Started",
                 text: "Hold on, we're working on your download. It should start anytime soon.",
@@ -29,9 +36,19 @@ for ( var button of downloadBtns ) {
 }
 
 
+// attach "click" event and listeners to the favourite-buttons to trigger alerts on click( if un-favourited )
+// else, if favourited, no alerts
 for ( var favButton of favouriteBtns ) {
+
+    // add event listener to each button
     favButton.addEventListener( "click", function( event ) {
+
+        // check if the image is a favourite ( or favourited ) by checking if the button contains ( "bg-red-600" ) class
+        // if so, alert and confirm from the user if they want to unfavourite the image and then unfavourite the image
+        // else, favourite the image by adding the ( "bg-red-600" ) class to the button
         if ( event.target.classList.contains("bg-red-600") ) {
+
+            // trigger an alert to the user to confirm the unfavourite action( based on sweetalert.js )
             var alert = Swal.fire(
                 {
                     title: "Confirm Action",
@@ -47,6 +64,7 @@ for ( var favButton of favouriteBtns ) {
             );
 
 
+            // if user chooses to unfavourite image, then unfavourite image ( based on sweetalert2 )
             alert.then( function( alertResult ) {
                 if ( alertResult.isConfirmed ) {
                     Swal.fire(
@@ -63,6 +81,8 @@ for ( var favButton of favouriteBtns ) {
                 }
             });
         } else {
+
+            // add the image as a favourite
             event.target.classList.add("bg-red-600")
         }
     });
